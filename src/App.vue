6a0259f227743897570c532eaf153938960d543b
AleScla <scla.alessandro@gmail.com>
2024-09-17 00:21:26 +0200

    stampati in pagina i film tramite ricerca, create 2 funzioni: una fa partire la ricerca api in base all'input inserito, l'altra pulisce l'array della precedente ricerca prima di avviarne una nuova, per evitare che si stampino più volte

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,15 @@
   3) Utilizzo del componente
 */
 // 1) Importazione del componente
+import {store} from './store.js';
 import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+
 
 export default {
   data() {
     return { 
-      count: 0
+      store
     }
   },
   // 2) Dichiarazione del componente
@@ -19,9 +22,24 @@ export default {
     AppHeader
   },
   methods: {
-    incrementCount() {
-      this.count++;
-    }
+    
+    StartAPI(){
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=e3bc7d75610ac968cc8e497e730d056c&language=it-IT',{
+        params: {
+          query: this.store.inputValue,
+        }
+      })
+      .then((res)=>{
+        console.log(res.data.results);
+        this.arrayClear()
+        store.allFilms.push(res.data.results)
+        console.log('allFilms:', store.allFilms)
+        // this.arrayClear()
+      })
+    },
+    arrayClear(){
+      store.allFilms = []
+    },
   }
 }
 </script>
@@ -31,10 +49,23 @@ export default {
     <!-- 3) Utilizzo del componente -->
     <AppHeader />
     
-    <main>
-      <button class="btn btn-primary" @click="incrementCount()">
-        {{ count }}
-      </button>
+    <main class="w-100">
+      <form @submit.prevent="StartAPI()" >
+        <input type="text" v-model="this.store.inputValue">
+        <button type="submit">
+          INVIA
+        </button>
+        
+        <div class="single-film w-100" v-for="(film, index) in store.allFilms" :key="index">
+          <div v-for="(singleFilm, i) in film" :key="i">
+            <div class="title w-100">{{ singleFilm.title }}</div>
+            <div class="original-title">{{ singleFilm.original_title }}</div>
+            <div class="language">{{ singleFilm.original_language }}</div>
+            <div class="rating">{{ Math.ceil((singleFilm.vote_average) / 2) }}</div>
+          </div>
+        </div>
+      </form>
+      
     </main>
   </div>
 </template>
