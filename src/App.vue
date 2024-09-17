cfc650bc89c534b4865bdd442254c87eff844f20
AleScla <scla.alessandro@gmail.com>
2024-09-17 12:15:30 +0200

    sistemata visualizzazione degli elementi, inserite anche serie  tv

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <script>
-/* 
-  Per importare ed utilizzare un componente dentro un altro devo SEMPRE seguire questi 3 passi:
-  1) Importazione del componente
-  2) Dichiarazione del componente
-  3) Utilizzo del componente
-*/
-// 1) Importazione del componente
+
 import {store} from './store.js';
 import AppHeader from './components/AppHeader.vue';
 import axios from 'axios';
@@ -22,7 +16,6 @@ export default {
     AppHeader
   },
   methods: {
-    
     StartAPI(){
       axios.get('https://api.themoviedb.org/3/search/movie?api_key=e3bc7d75610ac968cc8e497e730d056c&language=it-IT',{
         params: {
@@ -32,13 +25,22 @@ export default {
       .then((res)=>{
         console.log(res.data.results);
         this.arrayClear()
-        store.allFilms.push(res.data.results)
+        store.allFilms = res.data.results
         console.log('allFilms:', store.allFilms)
         // this.arrayClear()
+      }),
+      axios.get('https://api.themoviedb.org/3/search/tv?api_key=e3bc7d75610ac968cc8e497e730d056c&language=it-IT',{
+        params:{
+          query: this.store.inputValue
+        }
+      })
+      .then((res)=> {
+        console.log(res.data.results)
+        store.allSeries = res.data.results
       })
     },
     arrayClear(){
-      store.allFilms = []
+      store.inputValue = ''
     },
   }
 }
@@ -55,17 +57,25 @@ export default {
         <button type="submit">
           INVIA
         </button>
+          
         
-        <div class="single-film w-100" v-for="(film, index) in store.allFilms" :key="index">
-          <div v-for="(singleFilm, i) in film" :key="i">
-            <div class="title w-100">{{ singleFilm.title }}</div>
-            <div class="original-title">{{ singleFilm.original_title }}</div>
-            <div class="language">{{ singleFilm.original_language }}</div>
-            <div class="rating">{{ Math.ceil((singleFilm.vote_average) / 2) }}</div>
-          </div>
-        </div>
       </form>
-      
+      <h2>FILMS</h2>
+      <ul v-for="(film, i) in store.allFilms" :key="i">
+            <li class="title w-100">Titolo: {{ film.title }}</li>
+            <li class="original-title">Titolo originale: {{ film.original_title }}</li>
+            <li class="language">Lingua originale: {{ film.original_language }}</li>
+            <li class="rating">Voto: {{ Math.ceil((film.vote_average) / 2) }}</li>
+            <hr>
+      </ul>
+      <h2>SERIES</h2>
+      <ul v-for="(series, i) in store.allSeries" :key="i">
+            <li class="title w-100">Titolo: {{ series.name }}</li>
+            <li class="original-title">Titolo originale: {{ series.original_name }}</li>
+            <li class="language">Lingua originale: {{ series.original_language }}</li>
+            <li class="rating">Voto: {{ Math.ceil((series.vote_average) / 2) }}</li>
+            <hr>
+      </ul>
     </main>
   </div>
 </template>
--- src/store.js
+++ src/store.js
@@ -2,5 +2,6 @@ import {reactive} from 'vue';
 
 export const store = reactive ({
     inputValue: "",
-    allFilms:[]
+    allFilms:[],
+    allSeries:[]
 })
